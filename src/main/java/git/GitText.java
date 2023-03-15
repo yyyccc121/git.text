51c49e09de74d8aa6b39a96e287b021b11192798
yyyccc121 <1092330842@qq.com>
2023-03-15 22:37:38 +0800

    third commit

--- src/main/java/git/GitText.java
+++ src/main/java/git/GitText.java
@@ -4,5 +4,6 @@ public class GitText {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
